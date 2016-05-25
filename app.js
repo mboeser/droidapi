f6aefb6d52d1957067660b5ef107408eeff83c44
mboeser <mboeser@users.noreply.github.com>
2016-05-25 09:34:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(helmet());
 
 app.get("/",function(req,res){
     var oid = parseInt(req.query.oid);
-    console.log(req.query.oid);
-    console.log(typeof oid);
+    console.log(req.query);
+    //console.log(typeof oid);
     if (!oid || isNaN(oid)) {
         res.send("hello world");
     } else {

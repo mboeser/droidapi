0008ccbedffc896d93c6c8163d05ddcaf37103c8
Mike Boeser <mboeser@yahoo.com>
2016-05-30 21:55:33 -0500

    update var relic

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('newrelic');
+var relic       = require('newrelic');
 var express     =   require("express");
 var app         =   express();
 var helmet      =   require('helmet');
@@ -14,6 +14,7 @@ var db = mongoose.model('oid', new Schema({
     ping: { type: Number, require: true, default: 0 }
 }));
 
+app.use(relic());
 app.use(cors());
 app.use(helmet());
 
